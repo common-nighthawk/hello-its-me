3bec388fe6c345c8cf1bda2b729d885d255f5ee3
Daniel <danielallendeutsch@gmail.com>
2017-02-07 15:22:42 -0800

    styles the landing page.

--- landing.go
+++ landing.go
@@ -13,11 +13,11 @@ func landing(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  tArgs := templates.Args{StyleSheet: "centered"}
-  
+  tArgs := templates.Args{StyleSheet: "landing"}
+
   templateHTMLTop.Execute(w, tArgs)
-  templates.WriteFigletBanner(w, "Hello, It's Me")
-  fmt.Fprint(w, "<a href=\"/login\">Log In</a>")
-  fmt.Fprint(w, "<a href=\"/signup\">Sign Up</a>")
+  templates.WriteTextBanner(w, "Hello, It's Me")
+  fmt.Fprint(w, "<div class='log-in'><a href=\"/login\">Log In</a></div>")
+  fmt.Fprint(w, "<div class='sign-up'><a href=\"/signup\">Sign Up</a></div>")
   templateHTMLBottom.Execute(w, tArgs)
 }
